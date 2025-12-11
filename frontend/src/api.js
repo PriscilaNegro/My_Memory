16d75c43cff3e0179238656b92d2d1d92a26e02d
PriscilaNegro <priscila.luzn@hotmail.com>
2025-12-11 15:28:26 -0300

    Configurações para deploy

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333/api", //backend port and routes
+  baseURL: "https://my-memory-shjw.onrender.com/api",
+  //"http://localhost:3333/api", //backend port and routes
   headers: {
     "Content-Type": "application/json",
   },
